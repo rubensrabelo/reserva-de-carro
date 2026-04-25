bb1ecda64eb8232ab08f1b7cfe68f112d5b2d158
Rubens Rabelo <rubensrabelo123@gmail.com>
2026-04-25 10:18:59 -0300

    backend(reservation): start project

---
+++ reservation/src/main/java/io/github/rubensrabelo/car/reservation/ReservationApplication.java
@@ -0,0 +1,13 @@
+package io.github.rubensrabelo.car.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationApplication.class, args);
+	}
+
+}
